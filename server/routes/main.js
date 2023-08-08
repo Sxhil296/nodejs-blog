52b01f934628d2bcab08efa26e98e0fb6efda526
Sahil <leviack629@gmail.com>
2023-08-08 14:10:10 +0530

    connected mongodb

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,17 +1,43 @@
 const express = require("express");
 const router = express.Router();
+const Post = require("../models/Post");
 
-//Routes
-router.get("", (req, res) => {
- const locals = {
+/**
+ * GET
+ * HOME
+ **/
+router.get("", async (req, res) => {
+  const locals = {
     title: "NodeJS Blog",
-    description: "Simple Blog created with NodeJS, ExpressJS and MongoDB."
- }
-  res.render("index", {locals});
+    description: "Simple Blog created with NodeJS, ExpressJS and MongoDB.",
+  };
+
+  try {
+    const data = await Post.find();
+    res.render('index', {locals, data})
+    
+  } catch (error) {
+    console.log(error)
+  }
+  
 });
+
+
+
+
+
 
 router.get("/about", (req, res) => {
   res.render("about");
 });
-
 module.exports = router;
+
+// function insertPostData() {
+//   Post.insertMany([
+//     {
+//       title: "Building a Blog",
+//       body: "This is the body text",
+//     },
+//   ]);
+// }
+// insertPostData();
